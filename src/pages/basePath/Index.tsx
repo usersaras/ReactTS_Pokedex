8370c39673584f91f869c56a95144389631ef2c0
usersaras <saras.sthapitshrestha@gmail.com>
2023-08-20 20:04:07 +0545

    feat: Update styling for homepage

--- src/pages/basePath/Index.tsx
+++ src/pages/basePath/Index.tsx
@@ -17,11 +17,15 @@ const Home = () => {
 
   const carouselSlideClasses = 'h-full mx-auto object-contain';
   const carouselSlides = [
-    <div className="w-full h-full flex items-center content-center">
+    <div className="w-full h-full flex items-center content-center relative">
       <img src={Charizard} alt="Charizard" className={carouselSlideClasses} />
+      <div className="h-[500px] w-[500px] border-8 border-orange-400 absolute -z-10 rounded-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-90"></div>
+      <div className="h-[400px] w-[400px] bg-orange-500 absolute -z-10 rounded-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-90"></div>
     </div>,
-    <div className="w-full h-full flex items-center content-center">
+    <div className="w-full h-full flex items-center content-center relative">
       <img src={Pikachu} alt="Pikachu" className={carouselSlideClasses} />
+      <div className="h-[400px] w-[400px] border-8 border-yellow-500 absolute -z-10 rounded-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-90"></div>
+      <div className="h-[300px] w-[300px] bg-yellow-500 absolute -z-10 rounded-full left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-90"></div>
     </div>,
   ];
 
@@ -30,19 +34,21 @@ const Home = () => {
       <CenteredContainer className="flex items-center h-full gap-14">
         <>
           <div className="flex flex-col gap-8 items-start w-2/5">
-            <p className="font-bold text-4xl text-gray-800">
-              Gotta Catch 'Em All!
+            <p className="font-bold text-6xl text-gray-800">
+              All is fair in love, war and{' '}
+              <span className="text-cyan-700">Pokemon battles!</span>
             </p>
             <p className="font-medium text-lg text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo in
-              delectus incidunt dicta est! Expedita earum quod culpa provident
-              adipisci.
+              We do have a lot in common. The same earth, the same air, the same
+              sky. Maybe if we started looking at what is the same, instead of
+              looking at what is different, well, who knows?
+              <span className="block text-right">- Meowth</span>
             </p>
-            <button className="py-2 px-4 rounded-full bg-black text-white">
+            <button className="py-3 px-5 font-medium text-lg rounded-full bg-cyan-600 text-white hover:bg-cyan-800 transition-all duration-150">
               Explore Pokemons
             </button>
           </div>
-          <div className="w-3/5">
+          <div className="w-1/2">
             <EmblaCarousel options={OPTIONS} slides={carouselSlides} />
           </div>
         </>
